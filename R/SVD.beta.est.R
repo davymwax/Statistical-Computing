92549a9528772a3e311c8de17e180aeebb3213dd
Mghanga David Mwakima <dmwakima@MMwaxAir13.local>
2023-02-01 09:35:57 -0800

    wrote the function that uses SVD to find OLS estimates

---
+++ R/SVD.beta.est.R
@@ -0,0 +1,24 @@
+#' OLS Estimates using SVD Decomposition
+#'
+#' @param X: A n by p design matrix (including intercept)
+#' @param y: A numeric vector of length n
+#'
+#' @return A numeric vector
+#' @export
+#'
+#' @examples
+#' 
+#' X <- matrix(c(16, 12, 28, 36, 12, 58, 77, 41, 28, 77, 138, 134, 36, 41, 134, 431), byrow =T, nrow=4)
+#' y <- c(7, 18, 4, 11)
+#' SVD.beta.est(X, y)
+#' 
+SVD.beta.est <- function(X, y){
+  SVD <- svd(X)
+  sigma <- as.matrix(SVD$d)
+  U <- SVD$u
+  V <- SVD$v
+  inv_sigma <- solve(sigma)
+  right_hand = matvecprod(inv_sigma, t(U) , y, W=F)
+  beta_hat = matvecprod(diag(4), diag(4), right_hand, W=F)
+  return(beta_hat)
+}
